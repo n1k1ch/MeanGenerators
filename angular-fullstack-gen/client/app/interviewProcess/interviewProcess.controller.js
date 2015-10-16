bc7ab5b08fa048d5c2f3f90208035b28c2f7669a
Nikita Cherevkov <ncherevkov@csod.com>
2015-10-16 15:41:18 +0300

    implement ability to give answer to specific question, not in order

--- angular-fullstack-gen/client/app/interviewProcess/interviewProcess.controller.js
+++ angular-fullstack-gen/client/app/interviewProcess/interviewProcess.controller.js
@@ -7,9 +7,9 @@ angular.module('angularFullstackDemoApp')
     $scope.current = null;
 
     $http.get('/api/interviews/' + $stateParams.id)
-      .then(function(response) {
+      .then(function (response) {
         $scope.interview = response.data;
-      }, function(err) {
+      }, function (err) {
         console.log(err);
       });
 
@@ -18,68 +18,118 @@ angular.module('angularFullstackDemoApp')
         $scope.answers = response.data;
         console.log($scope.answers);
 
-        angular.forEach($scope.answers, function(el) {
+        angular.forEach($scope.answers, function (el) {
           el.given = false;
         });
       }, function (err) {
         console.log(err);
       });
 
-    $scope.start = function() {
-      $scope.interview.startedAt = new Date();
-      startAnswer(0);
-    };
-
-
-    $scope.giveAnswer = function() {
-      var index = $scope.answers.indexOf($scope.current);
-      $scope.current.given = true;
-      $scope.current.finishedAt = new Date();
-      index++;
-
-      if(index < $scope.answers.length) {
-        //go next
-        startAnswer(index);
-      } else {
-        //finish
-        $scope.current = null;
-        finishInterview();
-      }
-    };
-
-    $scope.isLastAnswer = function() {
-      return $scope.answers.indexOf($scope.current) === $scope.answers.length;
-    };
-
-    function startAnswer(index) {
-      $scope.current = $scope.answers[index];
-      $scope.current.rate =  $scope.current.rate || 0;
+    function startAnswer(answer) {
+      $scope.current = answer;
+      $scope.current.rate = $scope.current.rate || 0;
       $scope.current.startedAt = new Date();
+    }
+
+    function findNextQuestion(index) {
+      for (var i = index; i < $scope.answers.length; i++) {
+        var possibleNextAnswer = $scope.answers[i];
+
+        if (!possibleNextAnswer.given) {
+          return possibleNextAnswer;
+        }
+      }
+
+      return null;
     }
 
     function finishInterview() {
       $scope.interview.finishedAt = new Date();
       console.log($scope.answers);
       console.log($scope.interview);
-      $http.put('/api/interviews/finish/' + $scope.interview._id, {interview:$scope.interview, answers: $scope.answers})
-        .then(function(response) {
+      $http.put('/api/interviews/finish/' + $scope.interview._id, {
+        interview: $scope.interview,
+        answers: $scope.answers
+      })
+        .then(function (response) {
           console.log(response.data);
-        }, function(err) {
+        }, function (err) {
           console.log(err);
         });
     }
+
+    $scope.start = function () {
+      $scope.interview.startedAt = $scope.interview.startedAt || new Date();
+      var question = findNextQuestion(0);
+
+      if(question) {
+        startAnswer(question);
+      }
+    };
+
+
+    $scope.giveAnswer = function () {
+      var index = $scope.answers.indexOf($scope.current);
+      $scope.current.given = true;
+      $scope.current.finishedAt = new Date();
+      index++;
+      $scope.current = findNextQuestion(index);
+
+      if (!$scope.current) {
+        //finish
+        var allOver = true;
+
+        angular.forEach($scope.answers, function (e) {
+          if (e.given === false) {
+            allOver = false;
+          }
+        });
+
+        if (allOver) {
+          finishInterview();
+        }
+      } else {
+        startAnswer($scope.current);
+      }
+    };
+
+    $scope.isLastAnswer = function () {
+      return $scope.answers.indexOf($scope.current) === $scope.answers.length;
+    };
+
+    $scope.startAnswer2 = function (answer) {
+      console.log('start answer 2');
+      if ($scope.interview.startedAt == null) {
+        $scope.interview.startedAt = new Date();
+      }
+
+      $scope.current = answer;
+      $scope.current.rate = $scope.current.rate || 0;
+      $scope.current.startedAt = new Date();
+      $scope.current.startedManually = true;
+    };
+
+    $scope.giveAnswer2 = function () {
+      $scope.current.given = true;
+      $scope.current.finishedAt = new Date();
+      $scope.current = null;
+
+      if(!findNextQuestion(0)) {
+        finishInterview();
+      }
+    };
   })
 
-.directive('labelRating', function() {
+  .directive('labelRating', function () {
     return {
       restrict: 'A',
-      link: function(scope, element, attrs) {
+      link: function (scope, element, attrs) {
         element.addClass('label');
 
-        var stopObserve = attrs.$observe('labelRating', function(interpolatedValue) {
+        var stopObserve = attrs.$observe('labelRating', function (interpolatedValue) {
           element.html(interpolatedValue);
 
-          if(interpolatedValue <= 3) {
+          if (interpolatedValue <= 3) {
             element.removeClass('label-warning label-success').addClass('label-danger');
           } else if (interpolatedValue >= 4 && interpolatedValue < 8) {
             element.removeClass('label-danger label-success').addClass('label-warning');
